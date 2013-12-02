eefb993804d187a55aad2d726105513da0984314
joshwiker14 <joshwiker14@gmail.com>
2013-12-02 11:43:42 -0700

    Corrected main.java (it now compiles).

--- Persons.java
+++ Persons.java
@@ -1,51 +1,54 @@
+package partymanager;
+
 import java.util.ArrayList;
 
-
 public class Persons {
-	
-	private String fullName;
-	private ArrayList<String> likes;
-	private ArrayList<String> dislikes;
 
-	public People(String fName, String lName){
-		fullName = fName + " " + lName; 
-	}
-	
-	public void setFullName (String name, String name2){
-		this.fullName = name+ " " + name2;
-	}
-	
-	public ArrayList<String> getLikesList() {
-		return Likes;
-	}
-	
-	public void setLikes (ArrayList<String> food2){
-		this.likes = food2;
-	}
-	
-	public String getFullName(){
-		return this.fullName;
-	}
-	
-	public ArrayList<String> getDislikesList() {
-		return dislikes;
-	}
+    private String fullName;
+    private ArrayList<String> likes;
+    private ArrayList<String> dislikes;
 
-	public void setDislikes(ArrayList<String> dislikes) {
-		this.dislikes = dislikes;
-	}
-	public void addLike(String food){
-		likes.add(food);
-	}
-	public void removeLike(String food){
-		likes.remove(food);
-	}
-	
-	public void addDislike(String food) {
-		dislikes.add(food);
-	}
-	
-	public void removeDislike(String food) {
-		dislikes.remove(food);
-	}
+    public Persons(String fName, String lName) {
+        setFullName(fName, lName);
+    }
+
+    public void setFullName(String name, String name2) {
+        this.fullName = name + " " + name2;
+    }
+
+    public ArrayList<String> getLikesList() {
+        return likes;
+    }
+
+    public void setLikes(ArrayList<String> food2) {
+        this.likes = food2;
+    }
+
+    public String getFullName() {
+        return this.fullName;
+    }
+
+    public ArrayList<String> getDislikesList() {
+        return dislikes;
+    }
+
+    public void setDislikes(ArrayList<String> dislikes) {
+        this.dislikes = dislikes;
+    }
+
+    public void addLike(String food) {
+        likes.add(food);
+    }
+
+    public void removeLike(String food) {
+        likes.remove(food);
+    }
+
+    public void addDislike(String food) {
+        dislikes.add(food);
+    }
+
+    public void removeDislike(String food) {
+        dislikes.remove(food);
+    }
 }
--- Supplies.java
+++ Supplies.java
@@ -1,49 +1,59 @@
-public class Supplies
-{
-   private String Item;
-   private int Amount;
-   private String whatsItFor;
-   // party favor, food, decorations, cany, etc.
-   private String ItemType;
-   //constructor
-   
-   public void setItem(String myItem)
-   {
-      Item = myItem;
-   }
-   
-   public String getItem()
-   {
-      return Item;
-   }
-   
-   public void setAmount(int myAmount)
-   {
-      Amount = myAmount;
-   }
-   
-   public int getAmount()
-   {
-      return Amount;
-   }
-   
-   public void setWhatsItFor(String myPurpose)
-   {
-      whatsItFor = myPurpose;
-   }
-   
-   public String getWhatsItFor()
-   {
-      return whatsItFor;
-   }
-   
-   public void setItemType(String myType)
-   {
-      ItemType = myType;
-   }
-   
-   public String getItemType()
-   {
-      return ItemType;
-   }
+package partymanager;
+
+public class Supplies {
+
+    private String item;
+    private int amount;
+    private String whatsItFor;
+    // party favor, food, decorations, cany, etc.
+    private String itemType;
+    
+    //constructor
+
+    /**
+     *
+     * @param item
+     * @param itemType
+     * @param amount
+     * @param whatsItFor
+     */
+        public Supplies(String item,String itemType, int amount, String whatsItFor)
+    {
+        this.item = item;
+        this.itemType = itemType;
+        this.amount = amount;
+        this.whatsItFor = whatsItFor;
+    }
+    
+    public void setItem(String myItem) {
+        item = myItem;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setAmount(int myAmount) {
+        amount = myAmount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setWhatsItFor(String myPurpose) {
+        whatsItFor = myPurpose;
+    }
+
+    public String getWhatsItFor() {
+        return whatsItFor;
+    }
+
+    public void setItemType(String myType) {
+        itemType = myType;
+    }
+
+    public String getItemType() {
+        return itemType;
+    }
 }
